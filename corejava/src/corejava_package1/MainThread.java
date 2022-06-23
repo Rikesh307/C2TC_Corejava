fe664139ddd0116ed64909fe62c047d258ee4f59
rikesh sunuwar <rikesh sunuwar@Rikesh>
2022-06-23 11:27:36 +0530

    multithread

--- corejava/src/corejava_package1/MainThread.java
+++ corejava/src/corejava_package1/MainThread.java
@@ -1,17 +1,18 @@
 package corejava_package1;
 
-public class THREADEXMP {
+public class MainThread {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Thread daya = Thread.currentThread();
-		System.out.println("Thread is:"+daya);
-		System.out.println("Name of Thread is "+daya.getName());
+		System.out.println("Current Thread is:"+daya);
+		System.out.println("Name of Current Thread is "+daya.getName());
 		
 		daya.setName("dayanand");
 		daya.setPriority(5);
 		
-		System.out.println("After Change "+daya);
+		
+		System.out.println("After Changing name "+daya);
 		
 		System.out.println(daya.getName());
 	}
